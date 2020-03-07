721174c8e89fa0c54d0fdb25b9d1e74986f91928
Juan Guecaimburu <guecaimburu.j@gmail.com>
2020-03-07 15:33:42 +0100

    Parallax working on one element in both axis. Parallax smooth ending is debounced and working as well

---
+++ js/animations.js
@@ -0,0 +1,102 @@
+function selectParallaxElements () {
+  return Array.from(document.querySelectorAll('.parallax'))
+}
+
+function applyMainParallax () {
+  const scrolled = window.pageYOffset
+  parallaxElements.forEach(element => move(element, scrolled))
+}
+
+function move (element, scrolled) {
+  const rates = getParallaxRates(element)
+  const translateDistances = getTranslateDistances(scrolled, rates)
+  moveElementByDistances(element, translateDistances)
+}
+
+function getParallaxRates (element) {
+  const ratesString = element.dataset.rates
+  const rates = JSON.parse(ratesString)
+  return rates
+}
+
+function getTranslateDistances (scrolled, rates) {
+  const x = scrolled * rates.x
+  const y = scrolled * rates.y
+  return { x: x, y: y }
+}
+
+function moveElementByDistances (element, translateDistances) {
+  element.style.transform =
+  `translate3d(${translateDistances.x}px, ${translateDistances.y}px, 0px)`
+}
+
+function applyParallaxEnding () {
+  const scrolled = window.pageYOffset
+  const deltaScroll = getDeltaScroll(scrolled)
+  parallaxElements.forEach(element => moveSmooth(element, scrolled, deltaScroll))
+}
+
+function getDeltaScroll (scrolled) {
+  const deltaScroll = scrolled - lastScroll // global
+  lastScroll = scrolled
+  return deltaScroll
+}
+
+function moveSmooth (element, scrolled, deltaScroll) {
+  const rates = getParallaxRates(element)
+  const endingMvm = getEndingMovement(scrolled, deltaScroll, rates)
+  moveSmoothByDistance(element, endingMvm)
+}
+
+function getEndingMovement (scrolled, deltaScroll, rates) {
+  const endingDistance = 15
+  const endX = getProductSign(deltaScroll, rates.x) * endingDistance
+  const endY = getProductSign(deltaScroll, rates.y) * endingDistance
+  const x = scrolled * rates.x + endX
+  const y = scrolled * rates.y + endY
+  return { x: x, y: y }
+}
+
+function getProductSign (x, y) {
+  if (x * y > 0) {
+    return 1
+  } else if (x * y < 0) {
+    return -1
+  } else {
+    return 0
+  }
+}
+
+function moveSmoothByDistance (element, endingMvm) {
+  element.style.transition = 'transform 500ms ease-out'
+  element.style.transform = `translate3d(${endingMvm.x}px, ${endingMvm.y}px, 0px)`
+  flushCss(element)
+  element.style.transition = ''
+}
+
+function flushCss (element) {
+  element.offsetHeight
+}
+
+const debounce = (func, delay) => {
+  let inDebounce
+  return function () {
+    clearTimeout(inDebounce)
+    inDebounce = setTimeout(() => { func() }, delay)
+  }
+}
+
+// Run script
+
+console.log('Load Parallax script')
+
+let lastScroll = 0
+const parallaxElements = selectParallaxElements()
+
+window.addEventListener('scroll', function (e) { applyMainParallax() })
+
+window.addEventListener('scroll', debounce(() => {
+  applyParallaxEnding()
+}, 150))
+
+console.log('Parallax script end')
